5791bbdd15ad20f77d8ba57bf08527e2ee1dea65
דלדל <@DELL>
2018-05-02 00:51:21 +0300

    schedule done

--- student Calender Manager/src/TimeTableManager/Schedule.java
+++ student Calender Manager/src/TimeTableManager/Schedule.java
@@ -1,8 +1,23 @@
 package TimeTableManager;
 
+import java.util.ArrayList;
+
 public class Schedule {
 	
 	ArrayList<ArrayList<Event>> _table;
 	ArrayList<Restriction> _restrictions;
+	double grade;
+	
+	Schedule(ArrayList<ArrayList<Event>> _table, ArrayList<Restriction> _restriction) {
+		this._table = new ArrayList<ArrayList<Event>>(_table);
+		this._restrictions = new Array<Restriction>(_restriction);
+		grade = 0;
+	}
+	
+	void calcGrade() {
+		for(Restriction restriction : _restrictions) {
+			grade += restriction.calcGrade();
+		}
+	}
 	
 }
