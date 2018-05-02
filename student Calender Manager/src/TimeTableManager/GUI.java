a826a25c4f30bd8b5dfe0021c951d0bd028907b7
user154 <user154@gmail.com>
2018-05-02 04:07:49 +0300

    GUI added

---
+++ student Calender Manager/src/TimeTableManager/GUI.java
@@ -0,0 +1,119 @@
+package TimeTableManager;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.TextField;
+import java.awt.Label;
+
+public class GUI {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 717, 456);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnImportFile = new JButton("Import File");
+		btnImportFile.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnImportFile.setBounds(6, 6, 117, 29);
+		frame.getContentPane().add(btnImportFile);
+		
+		JButton btnAddRestrication = new JButton("Add restrication");
+		btnAddRestrication.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnAddRestrication.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAddRestrication.setBounds(6, 108, 117, 35);
+		frame.getContentPane().add(btnAddRestrication);
+		
+		JButton btnRefresh = new JButton("Refresh");
+		btnRefresh.setBounds(6, 155, 117, 29);
+		frame.getContentPane().add(btnRefresh);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnClear.setBounds(6, 196, 117, 29);
+		frame.getContentPane().add(btnClear);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnExit.setBounds(6, 401, 117, 29);
+		frame.getContentPane().add(btnExit);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(124, 0, 2, 430);
+		frame.getContentPane().add(panel);
+		
+		JButton btnPrint = new JButton("Print");
+		btnPrint.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new TableExample();
+			}
+		});
+		btnPrint.setBounds(6, 77, 117, 29);
+		frame.getContentPane().add(btnPrint);
+		
+		
+
+	}
+}
---
+++ student Calender Manager/src/TimeTableManager/TableExample.java
@@ -0,0 +1,21 @@
+package TimeTableManager;
+import javax.swing.*;    
+public class TableExample {    
+	JFrame f;    
+	TableExample(){    
+		f=new JFrame();    
+		String data[][]={ {"09:00-10:00","Amit \n\b 51.2.51","670000","101","Amit","670000","g"},    
+				{"10:00-11:00","Jai","780000","102","Jai","780000","t"},    
+				{"11:00-12:00","Sachin","700000","101","Sachin","700000","f"}};    
+		String column[]={"Hour","Sunday","Monday","Thuresday","Thuesday","Wendesday","Friday"};         
+		JTable jt=new JTable(data,column);    
+		jt.setBounds(30,40,500,600);     
+		JScrollPane sp=new JScrollPane(jt);    
+		f.add(sp);          
+		f.setSize(600,500);    
+		f.setVisible(true);    
+	}     
+	public static void main(String[] args) {    
+		    new TableExample();
+	}    
+}  
